1e845d361e74c958f025cb210a2059280c366f45
Subhangee Sahoo <shubh190601@gmail.com>
2025-01-13 19:12:51 +0530

    Customer Who Visited But Did Not Make Any Transaction

---
+++ Day4/Solution1.sql
@@ -0,0 +1,6 @@
+select customer_id,count(a.customer_id) as count_no_trans
+from Visits as a
+left join Transactions as b
+on a.visit_id = b.visit_id
+where b.visit_id is null
+group by a.customer_id
