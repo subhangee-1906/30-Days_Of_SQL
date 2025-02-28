6006ccb3de76140daf365a8e299b25d5d3c7d8c9
Subhangee Sahoo <shubh190601@gmail.com>
2025-02-28 23:18:42 +0530

    Occupations

---
+++ Day26/Solution_1.sql
@@ -0,0 +1,12 @@
+/*
+Enter your query here.
+*/
+SELECT MAX(CASE WHEN Occupation = 'Doctor' THEN Name END)
+AS Doctor, MAX(CASE WHEN Occupation = 'Professor' 
+               THEN Name END) AS Professor,
+               MAX(CASE WHEN Occupation = 'Singer'
+                   THEN Name END) AS Singer, 
+                   MAX(CASE WHEN Occupation = 'Actor' 
+                       THEN Name END) AS Actor 
+                       FROM ( SELECT Name, Occupation, ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name)
+                             AS RowNum FROM OCCUPATIONS ) AS RankedData GROUP BY RowNum ORDER BY RowNum;
